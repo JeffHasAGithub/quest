78a59921795b6a1b2d3be0ee5d5bd4bcb378ff25
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-28 20:40:09 -0600

    Add Response class to quest.methods module

--- quest/methods.py
+++ quest/methods.py
@@ -4,3 +4,9 @@ methods.py
 The methods module contains functions that implement
 the various actions/verbs used in the HTTP protocol.
 """
+
+from typing import NamedTuple
+
+
+class Response(NamedTuple):
+    status: int
