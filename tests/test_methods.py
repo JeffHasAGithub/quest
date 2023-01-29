2614d44a3042e5c7ebed25b53f56891460efb87f
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-28 21:19:34 -0600

    Add mock urlopen function as attribute in tests.test_methods.TestMethods class

--- tests/test_methods.py
+++ tests/test_methods.py
@@ -5,12 +5,21 @@ Tests for the quest.methods module"
 """
 
 import unittest
-import quest.methods as methods
+from unittest.mock import Mock, patch
+from quest.methods import get
 
 _test_url = "https://test.com"
+_mock_funcname = "quest.methods.urlopen"
 
 
 class TestMethods(unittest.TestCase):
+    def setUp(self):
+        self.mock_urlopen_patch = patch(_mock_funcname)
+        self.mock_urlopen = self.mock_urlopen_patch.start()
+
     def test_get(self):
-        response = methods.get(_test_url)
+        mock_func = self.mock_urlopen
+        mock_func.return_value.__enter__.return_value = Mock(status=200)
+
+        response = get(_test_url)
         self.assertEqual(response.status, 200)
