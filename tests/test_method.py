2523b5c9578a5fbe2682a868cd8fa4d73c3095f0
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-29 19:57:49 -0600

    Rename methods module (#4)
    * Copy 'methods' module to 'method' module
    
    * Fix import in 'test_method' module to use 'method' module
    
    * Fix import used in 'urlopen' patcher
    
    * Remove 'methods' module and corresponding test module

--- tests/test_method.py
+++ tests/test_method.py
@@ -7,11 +7,11 @@ Tests for the quest.methods module"
 import unittest
 from unittest.mock import Mock, patch
 from urllib.error import URLError, HTTPError
-from quest.methods import get, post
+from quest.method import get, post
 from quest.error import UrlError, HttpError
 
 _test_url = "https://test.com"
-_mock_funcname = "quest.methods.urllib.request.urlopen"
+_mock_funcname = "quest.method.urllib.request.urlopen"
 
 
 class TestGet(unittest.TestCase):
