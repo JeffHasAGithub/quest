af88d886d24220b7a776361ab5304fff10924120
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-29 19:27:10 -0600

    Add 'error' module

---
+++ quest/error.py
@@ -0,0 +1,13 @@
+"""
+error.py
+
+The error module contains class definitions for
+exceptions raised by the quest package.
+"""
+
+
+class QuestError(Exception):
+    """
+    The abstract base class inherited by
+    all errors raised within the package.
+    """
