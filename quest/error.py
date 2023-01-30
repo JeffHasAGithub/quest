4910b537952fa3e69a937019f7cf03c33f6269f0
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-29 19:43:50 -0600

    Add error module (#3)
    * Add 'error' module
    
    * Move 'HttpError' class from 'methods' module to 'error' module
    
    * Add docstring to 'HttpError' class in 'error' module
    
    * Copy 'UrlError' class from 'methods' module to 'error' module
    
    * Add docstring to 'UrlError' class in 'error' module
    
    * Replace local error classes from 'methods' module with classes from 'error' module
    
    * Fix imports in 'test_methods' module to use error classes from 'quest.error' module

---
+++ quest/error.py
@@ -0,0 +1,29 @@
+"""
+error.py
+
+The error module contains class definitions for
+exceptions raised by the quest package.
+"""
+
+
+class QuestError(Exception):
+    """
+    The abstract base class inherited by
+    all errors raised within the package.
+    """
+
+
+class HttpError(QuestError):
+    """
+    Raised when bad Http response is received.
+    """
+    def __init__(self, status: int):
+        self.status = status
+
+
+class UrlError(QuestError):
+    """
+    Raised when bad url is used in Http request.
+    """
+    def __init__(self, reason):
+        self.reason = reason
