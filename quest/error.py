bb8697e125b68c279ea1cecbd96957824f0aff3d
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-30 19:58:32 -0600

    Add '__str__' and '__repr__' methods to error classes

--- quest/error.py
+++ quest/error.py
@@ -11,8 +11,14 @@ class QuestError(Exception):
     The base class inherited by all
     errors raised within the package.
     """
-    def __init___(self, url: str):
+    def __init__(self, url: str):
         self.url = url
+
+    def __str__(self):
+        return f"QuestError: {self.url}"
+
+    def __repr__(self):
+        return f"<QuestError url='{self.url}'>"
 
 
 class HttpError(QuestError):
@@ -23,6 +29,12 @@ class HttpError(QuestError):
         super().__init__(url)
         self.status = status
 
+    def __str__(self):
+        return f"HttpError: {self.url} - {self.status}"
+
+    def __repr__(self):
+        return f"<HttpError url='{self.url}' status={self.status}>"
+
 
 class UrlError(QuestError):
     """
@@ -31,6 +43,12 @@ class UrlError(QuestError):
     def __init__(self, url: str):
         super().__init__(url)
 
+    def __str__(self):
+        return f"UrlError: {self.url}"
+
+    def __repr__(self):
+        return f"<UrlError url='{self.url}'>"
+
 
 class TimeoutError(QuestError):
     """
@@ -38,3 +56,9 @@ class TimeoutError(QuestError):
     """
     def __init__(self, url: str):
         super().__init__(url)
+
+    def __str__(self):
+        return f"TimeoutError: {self.url}"
+
+    def __repr__(self):
+        return f"<TimeoutError url='{self.url}'>"
