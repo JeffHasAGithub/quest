f91bf30c62463ff1fe0e1e8f145082f8e05cb2a4
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-30 21:37:38 -0600

    Add copy of function level 'headers' and 'data' vars to 'TestPost' attributes

--- tests/test_method.py
+++ tests/test_method.py
@@ -57,6 +57,9 @@ class TestPost(unittest.TestCase):
         self.mock_urlopen_patch = unittest.mock.patch(_mock_funcname)
         self.mock_urlopen = self.mock_urlopen_patch.start()
 
+        self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self.data = {"Name": "Jeff", "State": "Texas"}
+
     def test_post_200(self):
         mock_retv = self.mock_urlopen.return_value.__enter__.return_value
         mock_retv.status = 200
