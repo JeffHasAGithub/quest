05d538aa4732700a529f6b0fca7ed3711572afe8
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 17:54:15 -0600

    Add 'Response' class definition

--- quest/response.py
+++ quest/response.py
@@ -5,3 +5,10 @@ The response module contains definitions for the
 Response class and various helper functions for
 working with Http responses.
 """
+
+import typing
+
+
+class Response(typing.NamedTuple):
+    status: int
+    body: bytes
