bc28c0951a1767d66b4e8907b310f9485f7cfd6c
Jeff Moore <jeffmooreweb@gmail.com>
2023-01-31 18:29:51 -0600

    Add '_test_url' and '_mock_funcname' vars to 'common' module

--- tests/method/common.py
+++ tests/method/common.py
@@ -4,3 +4,6 @@ common.py
 Common definitions used in all modules
 of the tests.method subpackage"
 """
+
+_test_url: str = "https://test.com"
+_mock_funcname: str = "quest.method.urllib.request.urlopen"
