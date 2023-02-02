dbdad405eaf88e3ee90fa5094bf8ccd5eced09cd
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 18:46:54 -0600

    Add 'test_content_utf8' method to 'TestResponse' class

--- tests/test_response.py
+++ tests/test_response.py
@@ -30,3 +30,10 @@ class TestResponse(unittest.TestCase):
     def test_headers_invalid(self):
         bad_header_val = self.response.get_header("header_3")
         self.assertIsNone(bad_header_val)
+
+    def test_content_utf8(self):
+        default_arg = self.response.get_content()
+        with_arg = self.response.get_content("utf-8")
+
+        self.assertEqual(default_arg, "This is a response body")
+        self.assertEqual(with_arg, "This is a response body")
