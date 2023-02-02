e616b6c7e9e7e6ff3e450c269d56c1444e32e0e1
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 18:12:43 -0600

    Add 'TestResponse' class definition to 'test_response' module

--- tests/test_response.py
+++ tests/test_response.py
@@ -4,3 +4,10 @@ test_response.py
 Tests for the 'Response' class found
 in the quest.response module
 """
+
+import unittest
+import quest.response
+
+
+class TestResponse(unittest.TestCase):
+    pass
