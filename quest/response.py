6e91ea490b062ff84621b650db0d5e5c01b8740b
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 18:04:24 -0600

    'Response' class no longer inherits from 'NamedTuple' class

--- quest/response.py
+++ quest/response.py
@@ -6,11 +6,16 @@ Response class and various helper functions for
 working with Http responses.
 """
 
-import typing
 
+class Response():
 
-class Response(typing.NamedTuple):
-    url: str
-    status: int
-    headers: dict
-    content: bytes
+    def __init__(self, url: str, status: int,
+                 headers: dict = {}, content: bytes = b""):
+        self._url = url
+        self._status = status
+        self._headers = headers
+        self._content = content
+
+    @property
+    def headers(self, key: str):
+        return self.headers.get(key)
