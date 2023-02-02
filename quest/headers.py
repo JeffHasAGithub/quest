359fccbc35bafe9af92bc747d2fa32f0c52c9726
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 20:41:12 -0600

    Add headers module (#11)
    * Add 'headers' module to 'quest' package
    
    * Add module level docstring to 'headers' module
    
    * Add 'Headers' class definition to 'headers' module
    
    * Add constructor to 'Headers' class
    
    * Add 'get' method to 'Headers' class
    
    * Add helper functions for validating data passed to 'Header' class
    
    * Add 'set' method to 'Header' class
    
    * Add 'validate' method to 'Header' class
    
    * Add module level docstring to 'test_headers' module
    
    * Add 'TestHeaders' class definition
    
    * Add 'test_headers_set_valid' method to 'TestHeaders' class
    
    * Add 'test_headers_set_invalid' to 'TestHeaders' class
    
    * Replace local calls to 'headers' object with instance defined 'headers' object
    
    * Replace string literals with local variables 'key' and 'val' in 'test_headers_set_valid' method
    
    * Replace call to '_test_string' with '_test_kvpair' in 'Headers.validate' method
    
    * Rename 'validate' method in 'Headers' class to 'is_valid'
    
    * Add 'test_headers_is_valid' method to 'TestHeaders' class

---
+++ quest/headers.py
@@ -0,0 +1,33 @@
+"""
+headers.py
+
+The headers module contains definitions for the
+Headers class and various helper functions for
+working with the header entries found Http
+requests and response.
+"""
+
+
+class Headers():
+    def __init__(self):
+        self._headers: dict = {}
+
+    def get(self, key: str):
+        return self._headers.get(key)
+
+    def set(self, key: str, val: str):
+        if not _valid_kvpair(key, val):
+            raise ValueError(key, val)
+        self._headers[key] = val
+
+    def is_valid(self) -> bool:
+        return not any(((k, v) for k, v in self._headers.items()
+                        if not _valid_kvpair(k, v)))
+
+
+def _valid_string(string: any) -> bool:
+    return isinstance(string, str)
+
+
+def _valid_kvpair(key: any, val: any) -> bool:
+    return _valid_string(key) and _valid_string(val)
---
+++ tests/test_headers.py
@@ -0,0 +1,42 @@
+"""
+test_headers.py
+
+Tests for the 'Headers' class found
+in the quest.headers module
+"""
+
+import unittest
+import quest.headers
+
+
+class TestHeaders(unittest.TestCase):
+    def setUp(self):
+        self.headers = quest.headers.Headers()
+
+    def test_headers_set_valid(self):
+        key = "key1"
+        val = "val1"
+
+        self.headers.set(key, val)
+
+        self.assertIn(key, self.headers._headers)
+        self.assertEqual(self.headers.get(key), val)
+
+    def test_headers_set_invalid(self):
+        bad_key = 1
+        bad_val = ["not", "valid"]
+
+        with self.assertRaises(ValueError) as err_ctx:
+            self.headers.set(bad_key, bad_val)
+        self.assertIn(bad_key, err_ctx.exception.args)
+        self.assertIn(bad_val, err_ctx.exception.args)
+
+    def test_headers_is_valid(self):
+        bad_key = 1
+        good_key = "key1"
+        good_val = "val1"
+
+        self.headers._headers = {bad_key: good_val,
+                                 good_key: good_val}
+
+        self.assertFalse(self.headers.is_valid())
