ac48ce663303cb1e71b145fcf1850fa31734afd1
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 20:24:40 -0600

    Replace local calls to 'headers' object with instance defined 'headers' object

--- tests/test_headers.py
+++ tests/test_headers.py
@@ -10,20 +10,20 @@ import quest.headers
 
 
 class TestHeaders(unittest.TestCase):
-    def test_headers_set_valid(self):
-        headers = quest.headers.Headers()
-        headers.set("key1", "val1")
+    def setUp(self):
+        self.headers = quest.headers.Headers()
 
-        self.assertIn("key1", headers._headers)
-        self.assertEqual(headers.get("key1"), "val1")
+    def test_headers_set_valid(self):
+        self.headers.set("key1", "val1")
+
+        self.assertIn("key1", self.headers._headers)
+        self.assertEqual(self.headers.get("key1"), "val1")
 
     def test_headers_set_invalid(self):
-        headers = quest.headers.Headers()
-
         bad_key = 1
         bad_val = ["not", "valid"]
 
         with self.assertRaises(ValueError) as err_ctx:
-            headers.set(bad_key, bad_val)
+            self.headers.set(bad_key, bad_val)
         self.assertIn(bad_key, err_ctx.exception.args)
         self.assertIn(bad_val, err_ctx.exception.args)
