41c9c74fd1346a5631f1d8c6051470ca791a833a
Jeff Moore <jeffmooreweb@gmail.com>
2023-02-01 19:38:29 -0600

    Add module level docstring to 'headers' module

---
+++ quest/headers.py
@@ -0,0 +1,8 @@
+"""
+headers.py
+
+The headers module contains definitions for the
+Headers class and various helper functions for
+working with the header entries found Http
+requests and response.
+"""
